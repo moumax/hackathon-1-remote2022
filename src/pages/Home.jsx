c2b74557215584845c678afd132f64591d7965a3
Marc LANTOL <marc.lantol@gmail.com>
2022-05-12 09:29:26 +0200

    init

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import RandomApi from "../components/RandomApi";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <RandomApi />
+    </div>
+  );
+};
+
+export default Home;
