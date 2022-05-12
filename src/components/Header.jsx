b8d556fb7d2e2481644e66567351ff331d2c20e8
OulG <lou.gain@outlook.fr>
2022-05-12 23:54:47 +0200

    Create Navbar style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,42 @@
 import { NavLink } from "react-router-dom";
 import "../styles/NavBar.css";
-// const logo = require("../assets/img/veggies-logo.png");
+const logo = require("../assets/img/veggies-logo.png");
 
 function NavBar() {
+  const getActiveLinkStyle = ({ isActive }) => {
+    if (isActive) {
+      return {
+        color: "var(--accent-color)",
+        textDecoration: "underline",
+      };
+    }
+    return null;
+  };
     return (
+      <>
         <nav className="navbar">
           <div className="logo">
-            {/* <img src={logo} alt="logo"/> */}
+            <img src={logo} alt="logo"/>
             <h1>Foodie Score</h1>
           </div>
           <div className="pages">
-            <NavLink to="/">
-              Home
+            <NavLink to="/" style={getActiveLinkStyle}>
+              home
             </NavLink>
-            <NavLink to="/foodAnalysis">
-              Food Analysis
+            <p>・</p>
+            <NavLink to="/foodAnalysis" style={getActiveLinkStyle}>
+              food analysis
             </NavLink>
-            <NavLink to="/aboutUs">
-              About Us
+            <p>・</p>
+            <NavLink to="/aboutUs" style={getActiveLinkStyle}>
+              about us
             </NavLink>
           </div>
         </nav>
+        <div className="hr-solid">
+         <hr/>
+        </div>
+      </>
     );
   };
   
