6d2b07b3ce2df4ef622744087e985ddb9d915a90
Marc LANTOL <marc.lantol@gmail.com>
2022-05-12 12:17:07 +0200

    First api call

--- src/components/RandomApi.jsx
+++ src/components/RandomApi.jsx
@@ -2,25 +2,28 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const RandomApi = () => {
-  const [vehicle, setVehicle] = useState({});
-  const API = "https://random-data-api.com/api/vehicle/random_vehicle";
+  const [aliment, setAliment] = useState();
+  const [score, setScore] = useState();
+
+  const API =
+    "https://koumoul.com/data-fair/api/v1/datasets/agribalyse-synthese/lines?format=json&q_mode=simple&qs=6212";
 
   useEffect(() => {
-    const fetchVehicle = async () => {
-      try {
-        const response = await axios.get(API);
-        setVehicle(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchVehicle();
+    axios
+      .get(API)
+      .then((response) => response.data)
+      .then((data) => {
+        setAliment(data.results[0].Nom_du_Produit_en_Français);
+        setScore(data.results[0]["Score_unique_EF_(mPt/kg_de_produit)"]);
+      })
+      .catch((e) => console.error(e));
   }, []);
 
   return (
     <div>
-      <h1>Random API - vehicle</h1>
-      <h2>{vehicle.make_and_model}</h2>
+      <h1>Random API - La bouffe</h1>
+      <h2>{aliment}</h2>
+      <h3>{score}</h3>
     </div>
   );
 };
