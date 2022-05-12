6ef18f1e8b8965377c99f74dad4dfa8e5e943525
Marc LANTOL <marc.lantol@gmail.com>
2022-05-12 17:06:02 +0200

    Add the local Json and link it to our api call

--- src/components/RandomApi.jsx
+++ src/components/RandomApi.jsx
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+const list = require("../assets/list.json");
+
 const RandomApi = () => {
   const [aliment, setAliment] = useState();
   const [score, setScore] = useState();
+  const [idSelected, setIdSelected] = useState();
 
-  const API =
-    "https://koumoul.com/data-fair/api/v1/datasets/agribalyse-synthese/lines?format=json&q_mode=simple&qs=6212";
+  const filterLIst = (e) => {
+    setIdSelected(e.target.value);
+  };
+
+  const API = `https://koumoul.com/data-fair/api/v1/datasets/agribalyse-synthese/lines?format=json&q_mode=simple&qs=${idSelected}`;
 
   useEffect(() => {
+    for (let i = 0; i < list.length; i++) {
+      let obj = list[i];
+      console.log(`Id: ${obj}`);
+    }
     axios
       .get(API)
       .then((response) => response.data)
@@ -17,13 +27,17 @@ const RandomApi = () => {
         setScore(data.results[0]["Score_unique_EF_(mPt/kg_de_produit)"]);
       })
       .catch((e) => console.error(e));
-  }, []);
+  }, [idSelected]);
 
   return (
     <div>
       <h1>Random API - La bouffe</h1>
-      <h2>{aliment}</h2>
-      <h3>{score}</h3>
+      <select onChange={filterLIst}>
+        {list.map((acc) => (
+          <option key={acc.id}> {acc.no} </option>
+        ))}
+      </select>
+      <p>{score}</p>
     </div>
   );
 };
