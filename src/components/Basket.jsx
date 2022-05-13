e9a312c42405b53bb6c345f7018b90a6166ad0ca
FREDERIQUE DEMAS <frederique.demas73@gmail.com>
2022-05-13 09:59:46 +0200

    basket display

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -9,6 +9,26 @@ const Basket = () => {
       <div className="scoreitems">
         <h2>Your Foodie Score</h2>
       </div>
+      <div className="resize">
+          <article>item 1</article>
+          <article>item 2</article>
+          <article>item 3</article>
+          <article>item 4</article>
+          <article>item 5</article>
+          <article>item 6</article>
+          <article>item 7</article>
+          <article>item 8</article>
+          <article>item 9</article>
+          <article>item 10</article>
+          <article>item 11</article>
+          <article>item 12</article>
+          <article>item 13</article>
+          <article>item 14</article>
+          <article>item 15</article>
+          <article>item 16</article>
+          <article>item 17</article>
+          <article>item 18</article>
+      </div>
       <div className="counter">
         <p>GLOBAL SCORE</p>
       </div>
