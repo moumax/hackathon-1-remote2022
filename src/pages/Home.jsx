42504aa308ae68f031e376f5e6f16805d002a347
BignonEmma <emmamailpro@gmail.com>
2022-05-13 02:55:26 +0200

    the most part of the css is done, the hr style is still missing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import "../styles/App.css";
 import "../styles/variables.css";
 import "../styles/Home.css";
+const homeImage = require("../assets/img/image-home.jpg");
 
 const Home = () => {
   return (
@@ -15,20 +16,25 @@ const Home = () => {
             consequat. Duis aute irure dolor in reprehenderit in voluptate velit
             esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
             cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
+            anim id est laborum. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat.
           </p>
         </div>
-        <img src="../public/image-home.jpg" alt="hand with a plant" />
+        <img className="home-image" src={homeImage} alt="hand with a plant" />
       </div>
       <div className="container-quote">
         <div className="main-quote">
           <p>
-            “Healthy eating is a way of life, so it’s important to establish
-            routines that are simple, realistically, and ultimately livable.”
+            “<span className="strong-word">Healthy</span> eating is a way of
+            life, so it’s important to establish routines that are simple,
+            realistically, and ultimately livable.”
           </p>
         </div>
         <div className="author">
-          <p>Horace</p>
+          <p>— Horace.</p>
         </div>
         <hr className="little-separator" />
       </div>
